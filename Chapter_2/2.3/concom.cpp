e095dccff9e33cc2f34709a8dc5ce743ca0c9b36
Ricardo <ruizr274@gmail.com>
2022-08-17 13:07:35 -0400

    finished 2.3

--- Chapter_2/2.3/concom.cpp
+++ Chapter_2/2.3/concom.cpp
@@ -16,11 +16,44 @@ using namespace std;
 #define s second
 
 const int MOD = (int)1e9+7;
+const int N = 101;
+vector<pii>  G[100];
 
 int main()
 {
     ofstream fout ("concom.out");
     ifstream fin ("concom.in");
+    int n;
+    fin>>n;
+    set<int> comp;
+    FOR(i,n){
+        int u,v,p;
+        fin>>u>>v>>p;
+        comp.insert(u);
+        G[u].pb({v,p});
+    }
+    vector<pii> ans;
+    for(auto c: comp){
+        queue<int> q;
+        vi p(101,0);
+        vector<bool> vist(101,false);
+        q.push(c);
+        vist[c] = true;
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            if(u!=c) ans.pb({c,u});
+            for(pii v:G[u]){
+                p[v.f]+=v.s;
+                if(p[v.f]>50 && !vist[v.f]){
+                    q.push(v.f);
+                    vist[v.f] = true;
+                }
+            }
+        }
+    }
+    sort(ans.begin(),ans.end());
+    for(auto x: ans) fout<<x.f<<" "<<x.s<<endl;
+
 
     return 0;
 }
