9b69cea61204d25028a0ee55fb1b32f0fa5a8b57
Ricardo <ruizr274@gmail.com>
2022-08-17 14:50:09 -0400

    cleaned

---
+++ Chapter_2/2.4/ttwo.cpp
@@ -0,0 +1,25 @@
+/*
+ID: ruizr271
+TASK: ttwo
+LANG: C++                 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define f first
+#define s second
+
+const int MOD = (int)1e9+7;
+
+int main()
+{
+    ofstream fout ("ttwo.out");
+    ifstream fin ("ttwo.in");
+    return 0;
+}
